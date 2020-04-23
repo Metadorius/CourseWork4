87dc1957ebd59d22b7f7838515f5d567f6885028
Meta (X230T) <crabiter@vivaldi.net>
2020-04-23 17:13:33 +0300

    First models iteration

--- CourseWork4/CourseWork4/settings.py
+++ CourseWork4/CourseWork4/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'publications_app.apps.PublicationsAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- CourseWork4/publications_app/models.py
+++ CourseWork4/publications_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Cathedra(models.Model):
+    cathedra_number = models.IntegerField()
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=512)
+
+    def __str__(self):
+        return f'{self.title} ({self.cathedra_number})'
+
+
+class Position(models.Model):
+    position_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.position_name
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+    patronymic = models.CharField(max_length=64)
+    position = models.ForeignKey(Position, on_delete=models.PROTECT)
+    phone_number = PhoneNumberField(unique=True)
+    cathedra = models.ForeignKey(Cathedra, on_delete=models.PROTECT)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.surname} {self.name} {self.patronymic}'
